d8480d27c878e7c6c9ca22c62ce3e1c79f0e21bf
djshujja <djshujja@gmail.com>
2020-10-04 18:03:07 +0500

    shujja charts

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,108 +1,123 @@
-import React, { useState, useEffect } from "react";
-import { Line } from "react-chartjs-2";
-import axios from "axios";
-
-
-const Chart = () => {
-  const [chartData, setChartData] = useState({});
-  const [employeeSalary, setEmployeeSalary] = useState([]);
-  const [employeeAge, setEmployeeAge] = useState([]);
-
-  const chart = () => {
-    let empSal = [];
-    let empAge = [];
-    axios
-      .get(`https://aerbeti-bck-test.herokuapp.com/api/bookings`)
-      .then(res => {
-        console.log(res);
-        for (const dataObj of res.data) {
-          empSal.push(parseInt(dataObj.length));
-          empAge.push(parseInt(dataObj.date));
-        }
-        setChartData({
-          labels: empAge,
-          datasets: [
-            {
-              label: "level of thiccness",
-              data: empSal,
-              backgroundColor: ["rgba(75, 192, 192, 0.6)"],
-              borderWidth: 4
-            }
-          ]
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    console.log(empSal, empAge);
-  };
-
-  useEffect(() => {
-    chart();
-  }, []);
-  return (
-    <div className="App">
-      <h1>Dankmemes</h1>
-      <div>
-        <Line
-          data={chartData}
-          options={{
-            responsive: true,
-            title: { text: "THICCNESS SCALE", display: true },
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    autoSkip: true,
-                    maxTicksLimit: 10,
-                    beginAtZero: true
-                  },
-                  gridLines: {
-                    display: false
-                  }
-                }
-              ],
-              xAxes: [
-                {
-                  gridLines: {
-                    display: false
-                  }
-                }
-              ]
-            }
-          }}
-        />
-      </div>
-    </div>
-  );
-};
-
-export default Chart;
-// 
-// import React, {useEffect, useState}from 'react';
-// import {Line} from 'react-chartjs-2';
-
-// function Chart() {
-//     const [chartData,setchartData]=useState({})
-//     const chartjs=()=>{
-//         setchartData({
-//             labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul'],
-//             datasets: [{
-//                 label: 'abc',
-//                 data: [32,45,66,77,88,99,97],
-//                 // backgroundColor: [red],
-//                 borderWidth:4
-//             }]
+// import React, { useState, useEffect } from "react";
+// import { Line } from "react-chartjs-2";
+// import axios from "axios";
+//
+//
+// const Chart = () => {
+//   const [chartData, setChartData] = useState({});
+//   const [employeeSalary, setEmployeeSalary] = useState([]);
+//   const [employeeAge, setEmployeeAge] = useState([]);
+//
+//   const chart = () => {
+//     let empSal = [];
+//     let empAge = [];
+//     axios
+//       .get(`https://aerbeti-bck-test.herokuapp.com/api/bookings`)
+//       .then(res => {
+//         // console.log(res);
+//         // console.log(
+//         //   res.data.filter(a => a.createdAt.substr(0,10))
+//         // );
+//         let x;
+//         res.data.filter(ourData => {
+//           x = ourData.createdAt.includes('Oct')
+//           console.log(x)
+//           // empSal.push(parseInt(x))
+// }).map(filteredData => {
+//           console.log(filteredData)
+//
+//
+//         for (const dataObj of res.data) {
+//           empSal.push((res.data.length));
+//           empAge.push((dataObj.createdAt.substr(0,10)));
+//         }
 //         })
-//     }
-//     useEffect(()=>{
-//         chartjs()
-//     },[])
-//     return (
-//         <div>
-//             <Line data={chartData}></Line>
-//         </div>
-//     )
-// }
+//         setChartData({
+//           labels: empAge,
+//           datasets: [
+//             {
+//               label: "level of thiccness",
+//               data: empSal,
+//               backgroundColor: ["rgba(75, 192, 192, 0.6)"],
+//               borderWidth: 4
+//             }
+//           ]
+//         });
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//     console.log(empSal, empAge);
+//   };
+//
+//   useEffect(() => {
+//     chart();
+//   }, []);
+//   return (
+//     <div className="App">
+//       <h1>Leads Data</h1>
+//       <div>
+//         <Line
+//           data={chartData}
+//           options={{
+//             responsive: true,
+//             title: { text: "THICCNESS SCALE", display: true },
+//             scales: {
+//               yAxes: [
+//                 {
+//                   ticks: {
+//                     autoSkip: true,
+//                     maxTicksLimit: 10,
+//                     beginAtZero: true
+//                   },
+//                   gridLines: {
+//                     display: false
+//                   }
+//                 }
+//               ],
+//               xAxes: [
+//                 {
+//                   gridLines: {
+//                     display: false
+//                   }
+//                 }
+//               ]
+//             }
+//           }}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+//
+// export default Chart;
+//
+import React, {useEffect, useState}from 'react';
+import {Line} from 'react-chartjs-2';
 
-// export default Chart
+function Chart() {
+    const [chartData,setchartData]=useState({})
+    const chartjs=()=>{
+        setchartData({
+            labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul'],
+            datasets: [{
+                label: 'Leads',
+                data: [32,15,66,70,88,25,97],
+                              backgroundColor: ["rgba(75, 192, 192, 0.6)"],
+
+                // backgroundColor: [red],
+                borderWidth:4
+            }]
+        })
+    }
+    useEffect(()=>{
+        chartjs()
+    },[])
+    return (
+        <div>
+            <Line data={chartData}></Line>
+        </div>
+    )
+}
+
+export default Chart
--- src/pages/dealership/stats.jsx
+++ src/pages/dealership/stats.jsx
@@ -45,32 +45,32 @@ class Stats extends React.Component {
                         <Chart/>
                         <Grid container spacing={2}>
                             <Grid item xs={12} sm={6} md={3}>
-                                <AdminStatBadge 
-                                    badgeStyle = {{ backgroundColor: '#ff9801' }} 
+                                <AdminStatBadge
+                                    badgeStyle = {{ backgroundColor: '#ff9801' }}
                                     badgeCounter = {dealerStats ? dealerStats.total_cars : 0 }
                                     badgeText = "Total Cars"
                                     iconComponent = { <MdFindInPage color="#fff" size={40} /> }
                                 />
                             </Grid>
                             <Grid item xs={12} sm={6} md={3}>
-                                <AdminStatBadge 
-                                    badgeStyle = {{ backgroundColor: '#f44236' }} 
+                                <AdminStatBadge
+                                    badgeStyle = {{ backgroundColor: '#f44236' }}
                                     badgeCounter =  {dealerStats ? dealerStats.total_clicked : 0 }
                                     badgeText = "Total Clicked"
                                     iconComponent = { <MdMouse color="#fff" size={40} /> }
                                 />
                             </Grid>
                             <Grid item xs={12} sm={6} md={3}>
-                                <AdminStatBadge 
-                                    badgeStyle = {{ backgroundColor: '#4db151' }} 
+                                <AdminStatBadge
+                                    badgeStyle = {{ backgroundColor: '#4db151' }}
                                     badgeCounter = {dealerStats ? dealerStats.total_leads : 0 }
                                     badgeText = "Leads"
                                     iconComponent = { <FaPodcast color="#fff" size={38} /> }
                                 />
                             </Grid>
                             <Grid item xs={12} sm={6} md={3}>
-                                <AdminStatBadge 
-                                    badgeStyle = {{ backgroundColor: '#2895f0' }} 
+                                <AdminStatBadge
+                                    badgeStyle = {{ backgroundColor: '#2895f0' }}
                                     badgeCounter = {dealerStats ? dealerStats.total_active : 0 }
                                     badgeText = "Active"
                                     iconComponent = { <FaCalculator color="#fff" size={36} /> }
@@ -80,8 +80,8 @@ class Stats extends React.Component {
                         <br />
                         <Grid container spacing={2}>
                             <Grid item xs={12} sm={6} md={3}>
-                                <AdminStatBadge 
-                                    badgeStyle = {{ backgroundColor: '#0cbaae' }} 
+                                <AdminStatBadge
+                                    badgeStyle = {{ backgroundColor: '#0cbaae' }}
                                     badgeCounter = {dealerStats ? dealerStats.total_deactivated : 0 }
                                     badgeText = "Total Deleted"
                                     iconComponent = { null }
